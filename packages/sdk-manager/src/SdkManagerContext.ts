c2bb5379c529a6cde27305b4229b795680051c2d
FSM1 <myankelev@gmail.com>
2023-10-20 13:51:13 +0200

    initial changes

---
+++ packages/sdk-manager/src/SdkManagerContext.ts
@@ -0,0 +1,148 @@
+import {
+  EVMAssetTransfer,
+  Environment,
+  EvmFee,
+  Fungible,
+  Transfer
+} from '@buildwithsygma/sygma-sdk-core';
+import { consume, createContext, provide } from '@lit/context';
+import { LitElement, html } from 'lit';
+import { customElement, property } from 'lit/decorators.js';
+import {
+  WalletManagerContext,
+  WalletManagerController
+} from '@builtwithsygma/sygmaprotocol-wallet-manager';
+import { BaseProvider, Web3Provider } from '@ethersproject/providers';
+import { UnsignedTransaction } from '@ethersproject/transactions';
+
+export type SdkManagerStatus =
+  | 'idle'
+  | 'initialized'
+  | 'transferCreated'
+  | 'approvalsCompleted'
+  | 'deposited'
+  | 'completed';
+
+export type SdkManagerState = {
+  assetTransfer: EVMAssetTransfer;
+  status: SdkManagerStatus;
+  transfer?: Transfer<Fungible>;
+  fee?: EvmFee;
+  approvalTxs?: UnsignedTransaction[];
+  depositTx?: UnsignedTransaction;
+
+  initialize: (provider: BaseProvider, env?: Environment) => Promise<void>;
+  createTransfer: (
+    provider: BaseProvider,
+    destinationChainId: number,
+    destinationAddress: string,
+    resourceId: string,
+    amount: string
+  ) => Promise<void>;
+};
+
+export const SdkManagerContext = createContext<SdkManagerState | undefined>(
+  'sdk-context'
+);
+
+export class SdkManager implements SdkManagerState {
+  assetTransfer: EVMAssetTransfer;
+  status: SdkManagerStatus;
+  transfer?: Transfer<Fungible>;
+  fee?: EvmFee;
+  approvalTxs?: UnsignedTransaction[];
+  depositTx?: UnsignedTransaction;
+
+  constructor() {
+    this.assetTransfer = new EVMAssetTransfer();
+    this.status = 'idle';
+  }
+
+  async initialize(
+    provider: BaseProvider,
+    env: Environment = Environment.MAINNET
+  ) {
+    await this.assetTransfer.init(provider, env);
+    this.status = 'initialized';
+  }
+
+  async createTransfer(
+    provider: BaseProvider,
+    destinationChainId: number,
+    destinationAddress: string,
+    resourceId: string,
+    amount: string
+  ) {
+    const sourceAddress = await (provider as Web3Provider)
+      .getSigner()
+      .getAddress();
+
+    const transfer = await this.assetTransfer.createFungibleTransfer(
+      sourceAddress,
+      destinationChainId,
+      destinationAddress,
+      resourceId,
+      amount
+    );
+
+    const fee = await this.assetTransfer.getFee(transfer);
+
+    const approvals = await this.assetTransfer.buildApprovals(transfer, fee);
+
+    this.transfer = transfer;
+    this.fee = fee;
+    this.approvalTxs = approvals;
+    this.status =
+      approvals.length > 0 ? 'transferCreated' : 'approvalsCompleted';
+  }
+}
+
+/**
+ * @name SdkManagerContextProvider
+ * @description This component is responsible for providing the SdkManagerController as a context to all its children.
+ */
+
+@customElement('sdk-manager-context-provider')
+export class SdkManagerContextProvider extends LitElement {
+  @consume({ context: WalletManagerContext, subscribe: true })
+  @property({ attribute: false })
+  walletManager?: WalletManagerController;
+
+  @provide({ context: SdkManagerContext })
+  sdkManagerContext = new SdkManager();
+
+  async initialize(env?: Environment) {
+    if (!this.walletManager?.provider) {
+      throw new Error('No wallet connected');
+    }
+
+    await this.sdkManagerContext.initialize(this.walletManager.provider, env);
+  }
+
+  async createTransfer(
+    destinationChainId: number,
+    destinationAddress: string,
+    resourceId: string,
+    amount: string
+  ) {
+    if (!this.walletManager?.provider) {
+      throw new Error('No wallet connected');
+    }
+
+    if (!(this.sdkManagerContext.status === 'initialized')) {
+      throw new Error('SdkManager not initialized');
+    }
+
+    await this.sdkManagerContext.createTransfer(
+      this.walletManager.provider,
+      destinationChainId,
+      destinationAddress,
+      resourceId,
+      amount
+    );
+  }
+
+  render() {
+    return html`<slot></slot>`;
+  }
+}
