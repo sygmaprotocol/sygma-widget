f9a87932f5ffe7961c551e644ff20e2293fa4816
Michael Yankelev <12774278+FSM1@users.noreply.github.com>
2023-12-14 13:43:59 +0100

    feat: SDK Manager package (#25)
    * initial changes
    
    * initial changes
    
    * add exports
    
    * update lock file
    
    * initialize sdk-manager
    
    * more lock file updates
    
    * fix deposit
    
    * fix approvals
    
    * remove unnecessary code
    
    * update lock file
    
    * chore: update lock file
    
    * Move types to separate file
    
    * update exports
    
    * fix network switching
    
    * update yarn
    
    * update lock file after yarn upgrade
    
    * add network valid flag
    
    * add connected callback
    
    * chore: pr review
    
    * chore: ethers as peer dependency and update on lock file
    
    * chore: fixating dependencies
    
    * chore: versiong yarn
    
    * chore: going to stable version of yarn
    
    * chore: update lock file
    
    * chore: update yarnrc file
    
    * update lock file
    
    * chore: removing checksum behaviour flag
    
    * chore: removing js docs comments, adding yarn folder to the gitignore
    
    * chore: changing to removeAllListeners
    
    * chore: change on gitignore
    
    * chore: remove reference to yarn release file
    
    * chore: change method names
    
    * chore: PR review, changes on constructor implementation
    
    * chore: update gitignore
    
    * chore: fixing and adding some more basic tests
    
    * initial changes
    
    * add exports
    
    * initialize sdk-manager
    
    * more lock file updates
    
    * update lock file
    
    * chore: update lock file
    
    * fix network switching
    
    * update yarn
    
    * update lock file after yarn upgrade
    
    * add connected callback
    
    * fix merge issues
    
    * widget basics
    
    * chore: init wallet manager on connectedCallback and not the constructor
    
    * chore: default parameter for network value
    
    * chore: pr review last comments
    
    * chore: rename networks property to network
    
    * chore: name of packages and uncomment publish workflow
    
    * fix merge
    
    * resolve some comments
    
    * fix lint
    
    * update lockfile
    
    * implement comments
    
    * fix linting
    
    * feat: update npmignore + peer dep for events dependency
    
    * feat: update lock file
    
    * fix: lock file and package.json file
    
    * fix: update lock file
    
    * address comments
    
    * rename methods for more clarity
    
    * update to latest sdk
    
    * fix linting
    
    * fix lint
    
    * update script
    
    * update script
    
    * add vite config and npmignore it
    
    * chore(main): release 0.1.0
    
    Release-As: 0.1.0
    
    ---------
    
    Co-authored-by: Nicolás Riquelme Guzmán <nrriquelme@gmail.com>

---
+++ packages/sdk-manager/src/index.ts
@@ -0,0 +1,8 @@
+export {
+  SdkManagerContext,
+  SdkManagerContextProvider
+} from './SdkManagerContextProvider';
+
+export { SdkManager } from './SdkManager';
+
+export type { SdkManagerState, SdkManagerStatus } from './types/index';
