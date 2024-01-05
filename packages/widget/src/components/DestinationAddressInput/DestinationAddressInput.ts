393bf6b2cd00e645a970e655ce62222b1026ac76
FSM1 <myankelev@gmail.com>
2024-01-05 18:33:10 +0100

    more fixes

--- packages/widget/src/components/DestinationAddressInput/DestinationAddressInput.ts
+++ packages/widget/src/components/DestinationAddressInput/DestinationAddressInput.ts
@@ -5,6 +5,10 @@ import {
   SdkManagerContext,
   SdkManager
 } from '@buildwithsygma/sygmaprotocol-sdk-manager';
+import {
+  WalletManagerContext,
+  WalletManagerController
+} from '@buildwithsygma/sygmaprotocol-wallet-manager';
 import { Domain } from '@buildwithsygma/sygma-sdk-core';
 
 @customElement('destination-address-input')
@@ -13,24 +17,69 @@ class DestinationAddressInput extends LitElement {
   @state()
   sdkManager?: SdkManager;
 
+  @consume({ context: WalletManagerContext, subscribe: true })
+  @state()
+  walletManager?: WalletManagerController;
+
+  @state()
+  sameAddress = false;
+
+  @state()
+  error?: string;
+
+  handleChange(e: Event) {
+    console.log('handleChange');
+    const target = e.target as HTMLInputElement;
+    this.sdkManager?.setDestinationAddress(target.value);
+    this.requestUpdate();
+  }
+
+  handleCheckboxChange(e: Event) {
+    console.log('handleCheckboxChange');
+    const target = e.target as HTMLInputElement;
+    this.sameAddress = target.checked;
+  }
+
   render() {
-    console.log(this.sdkManager);
-    const destinationNetwork = this.sdkManager?.assetTransfer.config
+    const destinationDomain = this.sdkManager?.assetTransfer.config
       .getDomains()
       .find(
         (domain: Domain) =>
           domain.chainId === this?.sdkManager?.destinationChainId
       );
 
-    console.log(destinationNetwork);
+    const sourceDomain = this.sdkManager?.assetTransfer.config
+      .getDomains()
+      .find(
+        (domain: Domain) =>
+          domain.chainId === this.sdkManager?.assetTransfer.config.chainId
+      );
 
-    return html`<div>
-      <label for="same-address">Use same address</label>
-      <input id="same-address" type="checkbox" />
-      <br />
-      <label for="destination-address">Destination Address</label>
-      <input id="destination-address" />
-    </div>`;
+    if (destinationDomain?.type === sourceDomain?.type) {
+      return html`<div>
+        <label for="same-address">Use same address</label>
+        <input
+          id="same-address"
+          type="checkbox"
+          @change=${this.handleCheckboxChange}
+          value=${this.sameAddress}
+
+        />
+        <br />
+        <label for="destination-address">Destination Address</label>
+        <input
+          id="destination-address"
+          @change=${this.handleChange}
+          ?disabled=${this.sameAddress}
+          value=${this.sameAddress ? this.walletManager?.accountData ?? '' : ''}
+        />
+      </div>`;
+    } else {
+      return html`<div>
+        <label for="destination-address">Destination Address</label>
+        <input id="destination-address" />
+      </div>`;
+    }
   }
 }
 
