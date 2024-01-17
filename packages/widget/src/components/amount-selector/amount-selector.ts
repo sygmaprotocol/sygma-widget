24746f5f5ecf66cfb6f800e6ea562e4a1d4927dd
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2024-01-16 23:41:07 -0300

    feat: upon clicking on max, render max value and trigger change event

--- packages/widget/src/components/amount-selector/amount-selector.ts
+++ packages/widget/src/components/amount-selector/amount-selector.ts
@@ -3,6 +3,8 @@ import { LitElement, html } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 import { styles } from './styles';
 import { when } from 'lit/directives/when.js';
+import { ifDefined } from 'lit/directives/if-defined.js';
+import { Ref, createRef, ref } from 'lit/directives/ref.js';
 
 @customElement('amount-selector')
 export default class AmountSelector extends LitElement {
@@ -39,9 +41,18 @@ export default class AmountSelector extends LitElement {
   })
   tokenName?: string;
 
-  // eslint-disable-next-line class-methods-use-this
+  @state({
+    hasChanged: (n, o) => n !== o
+  })
+  selectedAmount?: number;
+
+  inputRef: Ref<HTMLInputElement> = createRef();
+
   handleAmountChange(event: Event): void {
     const { value } = event.target as HTMLInputElement;
+
+    this.selectedAmount = Number.parseFloat(value);
+
     dispatchEvent(
       new CustomEvent('amount-selector-change', {
         detail: value,
@@ -51,16 +62,41 @@ export default class AmountSelector extends LitElement {
     );
   }
 
+  useMaxBalance() {
+    this.selectedAmount = Number.parseFloat(this.tokenBalance!);
+
+    (this.inputRef.value as HTMLInputElement).value = `${this.selectedAmount}`;
+    const event = new Event('input', {
+      bubbles: true
+    });
+
+    this.inputRef.value?.dispatchEvent(event);
+  }
+
+  renderBalance() {
+    return html`
+      <section class="balanceContent">
+        <span>${`${Number.parseFloat(this.tokenBalance!).toFixed(4)}`}</span>
+        <button class="maxButton" @click=${this.useMaxBalance}>Max</button>
+      </section>
+    `;
+  }
+
   render() {
     return html`
       <div class="amountSelectorContainer">
+      <section class="tokenBalanceSection">
         <label class="amountSelectorLabel">Amount to transfer</label>
+        ${when(this.tokenBalance, () => this.renderBalance())}
+        </section>
         <section class="amountSelectorSection">
           <input 
           type="text" 
           class="amountSelectorInput" 
           placeholder="0" 
           @input=${this.handleAmountChange}
+          value=${ifDefined(this.selectedAmount)}
+          ${ref(this.inputRef)}
            />
           <base-selector
             .entries=${this.resources}
@@ -68,12 +104,6 @@ export default class AmountSelector extends LitElement {
             .disabled=${this.disabled}
           ></base-selector>
           </section>
-          <section class="tokenBalanceSection">
-            ${when(
-              this.tokenBalance,
-              () => html`<span>${`Balance: ${this.tokenBalance}`}</span>`
-            )}
-            </section>
         </div>
       </div>
     `;
