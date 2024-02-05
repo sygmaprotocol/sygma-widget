cca3d232d7507dece002b6dc45450c617d71784f
Filip Štoković <filip.stokovic@gmail.com>
2024-02-05 16:29:46 +0100

    chore: shorter conditions + use function declaration instad of arrow (as everywhere else)

--- packages/widget/src/components/buttons/connect-wallet.ts
+++ packages/widget/src/components/buttons/connect-wallet.ts
@@ -52,9 +52,9 @@ export class ConnectWalletButton extends BaseComponent {
     }
   };
 
-  private onDisconnectClicked = (): void => {
+  private onDisconnectClicked(): void {
     this.walletController.disconnectWallet();
-  };
+  }
 
   private isWalletConnected(): boolean {
     return !!this.wallets.evmWallet || !!this.wallets.substrateWallet;
@@ -67,14 +67,14 @@ export class ConnectWalletButton extends BaseComponent {
     const substrateAccount = substrateWallet?.accounts[0];
     return html` <div class="connectWalletContainer">
       ${when(
-        evmWallet?.address !== undefined,
+        !!evmWallet?.address,
         () =>
           html`<span class="walletAddress" title=${evmWallet?.address ?? ''}
             >${greenCircleIcon} ${shortAddress(evmWallet?.address ?? '')}</span
           >`
       )}
       ${when(
-        substrateAccount !== undefined,
+        !!substrateAccount,
         () =>
           html`<span
             class="walletAddress"
