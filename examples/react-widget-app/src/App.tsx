1bd702ddb0b7195f68d6cd8e70d64c44e7602b8d
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2024-02-07 17:34:17 -0300

    chore: example react app

---
+++ examples/react-widget-app/src/App.tsx
@@ -0,0 +1,10 @@
+import { SygmaProtocolReactWidget } from '@buildwithsygma/sygmaprotocol-react-widget'
+
+function App() {
+
+  return (
+    <SygmaProtocolReactWidget />
+  )
+}
+
+export default App
