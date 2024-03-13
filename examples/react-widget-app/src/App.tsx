d0feab41e9106b6696669d22c7804f0e54a5c936
mj52951 <mj52951@fer.hr>
2024-03-13 17:10:17 +0100

    Modified widget UI page

--- examples/react-widget-app/src/App.tsx
+++ examples/react-widget-app/src/App.tsx
@@ -1,9 +1,25 @@
 import { SygmaProtocolReactWidget } from '@buildwithsygma/sygmaprotocol-react-widget'
+import './App.css';
+import sygmaLogo from './public/favicon.svg'
+import gitHubLogo from './public/git.png'
 
 function App() {
 
   return (
-    <SygmaProtocolReactWidget />
+    <div>
+      <div className="centered">
+        <SygmaProtocolReactWidget />
+      </div>
+      <div className="centered text-container">
+        <p>If you want to obtain testnet tokens, you can <br/> &nbsp; &nbsp; &nbsp; &nbsp; do it through the Sygma faucet <a href="https://docs.buildwithsygma.com/environments/testnet/obtain-testnet-tokens" target="_blank">here</a>.</p>
+      </div>
+      <footer className="centered footer">
+        <a href="https://buildwithsygma.com/" target="_blank"><img src={sygmaLogo} alt="Main Page" style={{ marginLeft: "20%" }}/>Website</a>
+        <a href="https://docs.buildwithsygma.com/" target="_blank"><img src={sygmaLogo} alt="Documentation" style={{ marginLeft: "10%" }} />Docs</a>
+        <a href="https://github.com/sygmaprotocol" target="_blank"><img src={gitHubLogo} alt="GitHub" style={{ width: "50px" }} />GitHub</a>
+      </footer>
+  </div>
+
   )
 }
 
