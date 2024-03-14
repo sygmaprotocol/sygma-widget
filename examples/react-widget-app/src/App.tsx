caaa5055e3893abe12c2abb226b0753c132abfdd
mj52951 <mj52951@fer.hr>
2024-03-14 10:00:10 +0100

    Added icon wrapper class

--- examples/react-widget-app/src/App.tsx
+++ examples/react-widget-app/src/App.tsx
@@ -14,9 +14,18 @@ function App() {
         <p>If you want to obtain testnet tokens, you can <br/> &nbsp; &nbsp; &nbsp; &nbsp; do it through the Sygma faucet <a href="https://docs.buildwithsygma.com/environments/testnet/obtain-testnet-tokens" target="_blank">here</a>.</p>
       </div>
       <footer className="centered footer">
-        <a href="https://buildwithsygma.com/" target="_blank"><img src={sygmaLogo} alt="Main Page" id="website-icon"/>Website</a>
-        <a href="https://docs.buildwithsygma.com/" target="_blank"><img src={sygmaLogo} alt="Documentation" id="docs-icon" />Docs</a>
-        <a href="https://github.com/sygmaprotocol" target="_blank"><img src={gitHubLogo} alt="GitHub" id="github-icon" />GitHub</a>
+        <div className="centered icon-wrapper">
+          <a href="https://buildwithsygma.com/" target="_blank"><img src={sygmaLogo} alt="Main Page" /></a>
+          <a href="https://buildwithsygma.com/" target="_blank"><span>Website</span></a>
+        </div>
+        <div className="centered icon-wrapper">
+          <a href="https://docs.buildwithsygma.com/" target="_blank"><img src={sygmaLogo} alt="Documentation"/></a>
+          <a href="https://docs.buildwithsygma.com/" target="_blank"><span>Docs</span></a>
+        </div>
+        <div className="centered icon-wrapper">
+          <a href="https://github.com/sygmaprotocol" target="_blank"><img src={gitHubLogo} alt="GitHub" id="github-icon"/></a>
+          <a href="https://github.com/sygmaprotocol" target="_blank"><span>GitHub</span></a>
+        </div>
       </footer>
   </div>
 
