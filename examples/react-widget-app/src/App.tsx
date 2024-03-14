50c1f10d763547f6250c954130ed5152de41bc78
mj52951 <mj52951@fer.hr>
2024-03-14 09:22:50 +0100

    Added ids for specific icons

--- examples/react-widget-app/src/App.tsx
+++ examples/react-widget-app/src/App.tsx
@@ -14,9 +14,9 @@ function App() {
         <p>If you want to obtain testnet tokens, you can <br/> &nbsp; &nbsp; &nbsp; &nbsp; do it through the Sygma faucet <a href="https://docs.buildwithsygma.com/environments/testnet/obtain-testnet-tokens" target="_blank">here</a>.</p>
       </div>
       <footer className="centered footer">
-        <a href="https://buildwithsygma.com/" target="_blank"><img src={sygmaLogo} alt="Main Page" style={{ marginLeft: "20%" }}/>Website</a>
-        <a href="https://docs.buildwithsygma.com/" target="_blank"><img src={sygmaLogo} alt="Documentation" style={{ marginLeft: "10%" }} />Docs</a>
-        <a href="https://github.com/sygmaprotocol" target="_blank"><img src={gitHubLogo} alt="GitHub" style={{ width: "50px" }} />GitHub</a>
+        <a href="https://buildwithsygma.com/" target="_blank"><img src={sygmaLogo} alt="Main Page" id="website-icon"/>Website</a>
+        <a href="https://docs.buildwithsygma.com/" target="_blank"><img src={sygmaLogo} alt="Documentation" id="docs-icon" />Docs</a>
+        <a href="https://github.com/sygmaprotocol" target="_blank"><img src={gitHubLogo} alt="GitHub" id="github-icon" />GitHub</a>
       </footer>
   </div>
 
