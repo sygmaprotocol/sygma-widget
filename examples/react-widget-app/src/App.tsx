80b4b4a7f9b36dc9dde71bbcb0dcb5c202177dc2
mj52951 <mj52951@fer.hr>
2024-03-18 09:13:41 +0100

    Made proposed changes

--- examples/react-widget-app/src/App.tsx
+++ examples/react-widget-app/src/App.tsx
@@ -1,7 +1,8 @@
 import { SygmaProtocolReactWidget } from '@buildwithsygma/sygmaprotocol-react-widget'
 import './App.css';
-import sygmaLogo from './public/favicon.svg'
-import gitHubLogo from './public/git.png'
+import sygmaIcon from './public/sygmaIcon.svg'
+import gitHubIcon from './public/githubIcon.png'
+import docsIcon from './public/docsIcon.png'
 
 function App() {
 
@@ -11,24 +12,23 @@ function App() {
         <SygmaProtocolReactWidget />
       </div>
       <div className="centered text-container">
-        <p>If you want to obtain testnet tokens, you can <br/> &nbsp; &nbsp; &nbsp; &nbsp; do it through the Sygma faucet <a href="https://docs.buildwithsygma.com/environments/testnet/obtain-testnet-tokens" target="_blank">here</a>.</p>
+        <p>If you want to obtain testnet tokens, <br/> you can do it through the Sygma faucet <a href="https://docs.buildwithsygma.com/environments/testnet/obtain-testnet-tokens" target="_blank">here</a>.</p>
       </div>
       <footer className="centered footer">
         <div className="centered icon-wrapper">
-          <a href="https://buildwithsygma.com/" target="_blank"><img src={sygmaLogo} alt="Main Page" /></a>
+          <a href="https://buildwithsygma.com/" target="_blank"><img src={sygmaIcon} alt="Main Page" /></a>
           <a href="https://buildwithsygma.com/" target="_blank"><span>Website</span></a>
         </div>
         <div className="centered icon-wrapper">
-          <a href="https://docs.buildwithsygma.com/" target="_blank"><img src={sygmaLogo} alt="Documentation"/></a>
+          <a href="https://docs.buildwithsygma.com/" target="_blank"><img src={docsIcon} alt="Documentation"/></a>
           <a href="https://docs.buildwithsygma.com/" target="_blank"><span>Docs</span></a>
         </div>
         <div className="centered icon-wrapper">
-          <a href="https://github.com/sygmaprotocol" target="_blank"><img src={gitHubLogo} alt="GitHub" id="github-icon"/></a>
+          <a href="https://github.com/sygmaprotocol" target="_blank"><img src={gitHubIcon} alt="GitHub" id="github-icon"/></a>
           <a href="https://github.com/sygmaprotocol" target="_blank"><span>GitHub</span></a>
         </div>
       </footer>
   </div>
-
   )
 }
 
