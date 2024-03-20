52e1f73e31887379c730d369cbebb7aba5604572
Saad Ahmed Siddiqui <saadahm.siddiqui@gmail.com>
2024-03-20 21:38:54 +0500

    test added

--- packages/widget/tests/unit/components/transfer-detail/transfer-detail.test.ts
+++ packages/widget/tests/unit/components/transfer-detail/transfer-detail.test.ts
@@ -12,9 +12,36 @@ import {
   ResourceType
 } from '@buildwithsygma/sygma-sdk-core';
 import { constants } from 'ethers';
+import { parseUnits } from 'ethers/lib/utils';
 import { FungibleTransferDetail } from '../../../../src/components/transfer/fungible/transfer-detail';
 
 describe('sygma-fungible-transfer-detail', function () {
+  const mockedFee: EvmFee = {
+    fee: constants.Zero,
+    type: FeeHandlerType.BASIC,
+    handlerAddress: ''
+  };
+
+  const mockedResource: EvmResource = {
+    resourceId: '',
+    type: ResourceType.FUNGIBLE,
+    address: ''
+  };
+
+  const mockedSourceDomainConfig: BaseConfig<Network> = {
+    id: 1,
+    chainId: 1,
+    name: 'ethereum',
+    type: Network.EVM,
+    bridge: '',
+    nativeTokenSymbol: 'ETH',
+    nativeTokenFullName: 'Ether',
+    nativeTokenDecimals: BigInt(18),
+    startBlock: BigInt(0),
+    blockConfirmations: 0,
+    resources: []
+  };
+
   afterEach(() => {
     fixtureCleanup();
   });
@@ -25,32 +52,6 @@ describe('sygma-fungible-transfer-detail', function () {
   });
 
   it('renders transfer-detail', async () => {
-    const mockedFee: EvmFee = {
-      fee: constants.Zero,
-      type: FeeHandlerType.BASIC,
-      handlerAddress: ''
-    };
-
-    const mockedResource: EvmResource = {
-      resourceId: '',
-      type: ResourceType.FUNGIBLE,
-      address: ''
-    };
-
-    const mockedSourceDomainConfig: BaseConfig<Network> = {
-      id: 1,
-      chainId: 1,
-      name: 'ethereum',
-      type: Network.EVM,
-      bridge: '',
-      nativeTokenSymbol: 'ETH',
-      nativeTokenFullName: 'Ether',
-      nativeTokenDecimals: BigInt(18),
-      startBlock: BigInt(0),
-      blockConfirmations: 0,
-      resources: []
-    };
-
     const el = await fixture(html`
       <sygma-fungible-transfer-detail
         .fee=${mockedFee}
@@ -64,5 +65,24 @@ describe('sygma-fungible-transfer-detail', function () {
     ) as HTMLElement;
 
     assert.isNotNull(transferDetail);
+  });
+
+  it('shows fee correctly', async () => {
+    const value = '1.0000 ETH';
+    mockedFee.fee = parseUnits('1');
+
+    const el = await fixture(html`
+      <sygma-fungible-transfer-detail
+        .fee=${mockedFee}
+        .selectedResource=${mockedResource}
+        .sourceDomainConfig=${mockedSourceDomainConfig}
+      ></sygma-fungible-transfer-detail>
+    `);
+
+    const transferDetail = el.shadowRoot!.querySelector(
+      '.transferDetailContainerValue'
+    ) as HTMLElement;
+
+    assert.include(transferDetail.innerHTML, value);
   });
 });
