023d44c4599991638e925414eb642aa46d20ac28
Nicolás Riquelme Guzmán <nrriquelme@gmail.com>
2024-04-08 11:04:57 -0400

    fix: update log

--- packages/widget/src/controllers/wallet-manager/manager.ts
+++ packages/widget/src/controllers/wallet-manager/manager.ts
@@ -105,7 +105,11 @@ export class WalletController implements ReactiveController {
           params: [{ chainId: utils.hexValue(chainId) }]
         });
       } catch (switchError) {
-        console.log('env', import.meta.env.VITE_CHAIN_ID_URL);
+        console.log(
+          'env',
+          import.meta.env.VITE_CHAIN_ID_URL,
+          import.meta.env.VITE_BRIDGE_ENV
+        );
         const chainData = (await (
           await fetch(import.meta.env.VITE_CHAIN_ID_URL)
         ).json()) as ChainDataResponse;
