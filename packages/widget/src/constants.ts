d5886bd9fb9a771364221d15f49669181123be42
Nicolás <nrriquelme@gmail.com>
2024-04-16 10:05:23 -0400

    feat: automatically add unknown source evm network into wallet  (#162)
    <!--- Provide a general summary of your changes in the Title above -->
    
    ## Description
    
    Whenever user switches to a unsuported network, upon swtich error we
    allow the user to add the network
    
    <!--- Describe your changes in detail -->
    
    ## Related Issue Or Context
    
    <!--- If suggesting a new feature or change, please discuss it in an
    issue first -->
    <!--- If fixing a bug, there should be an issue describing it with steps
    to reproduce -->
    <!--- Otherwise, describe context and motivation for change here -->
    
    Closes: #132 
    
    ## How Has This Been Tested? Testing details.
    
    <!--- Please describe in detail how you tested your changes. -->
    <!--- Include details of your testing environment, and the tests you ran
    to -->
    <!--- see how your change affects other areas of the code, etc. -->
    
    ## Types of changes
    
    <!--- What types of changes does your code introduce? Put an `x` in all
    the boxes that apply: -->
    
    - [ ] Bug fix (non-breaking change which fixes an issue)
    - [ ] New feature (non-breaking change which adds functionality)
    - [ ] Breaking change (fix or feature that would cause existing
    functionality to change)
    - [ ] Documentation
    
    ## Checklist:
    
    <!--- Go over all the following points, and put an `x` in all the boxes
    that apply. -->
    <!--- If you're unsure about any of these, don't hesitate to ask. We're
    here to help! -->
    
    - [ ] I have commented my code, particularly in hard-to-understand
    areas.
    - [ ] I have ensured that all acceptance criteria (or expected behavior)
    from issue are met
    - [ ] I have updated the documentation locally and in sygma-docs.
    - [ ] I have added tests to cover my changes.
    - [ ] I have ensured that all the checks are passing and green, I've
    signed the CLA bot
    
    ---------
    
    Signed-off-by: Marin Petrunic <marin.petrunic@gmail.com>
    Co-authored-by: Marin Petrunić <mpetrunic@users.noreply.github.com>
    Co-authored-by: Filip Štoković <59089574+sztok7@users.noreply.github.com>
    Co-authored-by: Anton Lykhoyda <lykhoyda@gmail.com>

--- packages/widget/src/constants.ts
+++ packages/widget/src/constants.ts
@@ -6,6 +6,7 @@ export const DEFAULT_ETH_DECIMALS = 18;
 export const MAINNET_EXPLORER_URL = 'https://scan.buildwithsygma.com/transfer/';
 export const TESTNET_EXPLORER_URL =
   'https://scan.test.buildwithsygma.com/transfer/';
+export const CHAIN_ID_URL = 'https://chainid.network/chains_mini.json';
 
 type WsUrl = `ws://${string}` | `wss://${string}`;
 export const SUBSTRATE_RPCS: {
